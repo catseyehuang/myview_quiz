0b4f61b49aa98f5e520408e271e112ff0e2b831d
Ciao <catseye.huang@gmail.com>
2026-03-26 15:50:37 +0800

    update gas web url

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,3 @@
 // /docs/config.js
 // 存放所有前端共用的設定，請將您的 Web App 網址更新在這裡。
-const gasApiUrl = "請將您重新部署後取得的 Web App 網址貼在這裡";
+const gasApiUrl = "https://script.google.com/macros/s/AKfycbxGCCwnUMw3yKNcEvhtc0MjdlQ0hNbrjjRtAvWqRne5ak8DFeb_epYMg78VqslRhCUp3w/exec";
