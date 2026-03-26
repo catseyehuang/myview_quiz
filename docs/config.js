af32c874fe96d55b5e6ac5079461d1bec0037768
Ciao <catseye.huang@gmail.com>
2026-03-26 16:45:36 +0800

    remove no need .js

--- docs/config.js
+++
@@ -1,3 +0,0 @@
-// /docs/config.js
-// 存放所有前端共用的設定，請將您的 Web App 網址更新在這裡。
-const gasApiUrl = "https://script.google.com/macros/s/AKfycbxGCCwnUMw3yKNcEvhtc0MjdlQ0hNbrjjRtAvWqRne5ak8DFeb_epYMg78VqslRhCUp3w/exec";
